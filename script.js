ddd60256a80c72a59814eba8d5ef17216d271114
Sharif-Mahmud <sharif230@gmail.com>
2024-05-30 23:38:44 +0600

    family incom

---
+++ script.js
@@ -0,0 +1,95 @@
+const balance = document.getElementById("balance");
+const income = document.getElementById("income");
+const expense = document.getElementById("expense");
+const list = document.getElementById("list");
+const form = document.getElementById("transaction-form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+
+let transactions = [];
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === "" || amount.value.trim() === "") {
+    alert("Please add a description and amount");
+    return;
+  }
+
+  const transaction = {
+    id: generateID(),
+    text: text.value,
+    amount: +amount.value,
+  };
+
+  transactions.push(transaction);
+
+  addTransactionDOM(transaction);
+  updateValues();
+  text.value = "";
+  amount.value = "";
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+  const sign = transaction.amount < 0 ? "-" : "+";
+  const item = document.createElement("li");
+
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+  item.innerHTML = `
+        ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
+    `;
+
+  list.appendChild(item);
+}
+
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  income.innerText = `$${income}`;
+  expense.innerText = `$${expense}`;
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM() {
+  list.innerHTML = "";
+
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+// Event listeners
+form.addEventListener("submit", addTransaction);
+
+updateDOM();
